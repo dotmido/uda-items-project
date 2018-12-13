6d4af6e86a921bf8416f361fc91644dd76d76712
Mohammed Mustafa <mjavax@gmail.com>
2018-12-13 19:20:57 +0300

    fix: import datetime

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import datetime
 
 Base = declarative_base()
 
